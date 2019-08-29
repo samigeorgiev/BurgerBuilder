d4a4ed91b32b72504da71923488e49efd3984648
Samuil Georgiev <sami.georgiev@gmail.com>
2019-08-29 14:06:43 +0300

    added burger ingredients

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import styles from './index.module.css';
+
 const layout = props => (
     <>
         <div>
             Toolbar, SideDrawer, Backdrop
         </div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
     </>
