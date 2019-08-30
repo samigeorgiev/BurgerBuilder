cac12a59f036bf0fe31e4b46a36e43ab276861c4
Samuil Georgiev <sami.georgiev@gmail.com>
2019-08-30 16:33:43 +0300

    added shouldCOmponentUpdate in modal

--- src/components/UI/Modal/index.js
+++ src/components/UI/Modal/index.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Backdrop from '../Backdrop';
 
 import styles from './index.module.css';
 
-const modal = props => (
-    <>
-        <Backdrop show={props.show} click={props.close} />
-        <div
-            className={styles.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
-    </>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.show !== nextProps.show;
+    }
+
+    render() {
+        return (
+            <>
+                <Backdrop show={this.props.show} click={this.props.close} />
+                <div
+                    className={styles.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </>
+        );
+    }
+}
+
+export default Modal;
