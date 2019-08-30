3c43eb76cbc4a7223356f4a83d8eb5de3156a265
Samuil Georgiev <sami.georgiev@gmail.com>
2019-08-30 11:40:47 +0300

    some changes in logo component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Toolbar from 'components/Navigation/Toolbar';
+import SideDrawer from 'components/Navigation/SideDrawer';
 
 import styles from './index.module.css';
 
 const layout = props => (
     <>
         <Toolbar />
+        <SideDrawer />
         <main className={styles.Content}>
             {props.children}
         </main>
--- src/components/Navigation/Toolbar/index.js
+++ src/components/Navigation/Toolbar/index.js
@@ -8,7 +8,9 @@ import styles from './index.module.css';
 const toolbar = props => (
     <header className={styles.Toolbar}>
         <div>MENU</div>
-        <Logo />
+        <div className={styles.Logo}>
+            <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
