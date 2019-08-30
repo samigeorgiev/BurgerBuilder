f045fe0d79fc40ad5b5c08dcc51d3fc8ea966ba6
Samuil Georgiev <sami.georgiev@gmail.com>
2019-08-30 13:44:53 +0300

    turn layout to class component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Toolbar from 'components/Navigation/Toolbar';
 import SideDrawer from 'components/Navigation/SideDrawer';
 
 import styles from './index.module.css';
 
-const layout = props => (
-    <>
-        <Toolbar />
-        <SideDrawer />
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </>
-);
+class Layout extends Component {
 
-export default layout;
+    render() {
+        return (
+            <>
+                <Toolbar />
+                <SideDrawer />
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </>
+        );
+    }
+}
+
+export default Layout;
