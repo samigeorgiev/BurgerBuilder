f145448134c4ff033fd779146fe093b1c4769acc
Samuil Georgiev <sami.georgiev@gmail.com>
2019-08-31 13:11:58 +0300

    added ejecting the interceptors

--- src/hoc/withErrorHandler/index.js
+++ src/hoc/withErrorHandler/index.js
@@ -9,14 +9,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
             this.state = {
                 error: null
             };
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, err => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, err => {
                 this.setState({error: err});
             });
         }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+        }
+        
         
         hideErrorHandler = () => {
             this.setState({error: null});
