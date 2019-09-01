db8e2d49fc32c7dcc1ca8c2902e735faed7e51b2
Samuil Georgiev <sami.georgiev@gmail.com>
2019-09-01 23:15:51 +0300

    started adding routing

---
+++ src/components/Order/CheckoutSummary/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const checkoutSummary = props => {
+    return (
+        <div>
+            <h1>Its delicious</h1>
+            <div style={{width: '300px', }}>
+
+            </div>
+        </div>
+    );
+};
+
+export default checkoutSummary;
