f50909525e29691cc2fc98d5c32c4a6897ecbbc9
Samuil Georgiev <sami.georgiev@gmail.com>
2019-09-04 09:38:30 +0300

    added checkout component + checkout summary

--- src/components/Order/CheckoutSummary/index.js
+++ src/components/Order/CheckoutSummary/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import Burger from 'components/Burger';
+import Button from 'components/UI/Button';
+
+import styles from './index.module.css';
+
 const checkoutSummary = props => {
     return (
-        <div>
-            <h1>Its delicious</h1>
-            <div style={{width: '300px', }}>
-
+        <div className={styles.CheckoutSummary}>
+            <h1>We hope it tastes well</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients} />
             </div>
+            <Button type="Danger" click={props.cancel}>CANCEL</Button>
+            <Button type="Success" click={props.continue}>CONTINUE</Button>
         </div>
     );
 };
